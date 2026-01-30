add6ed0ea9700b8c541554818a157f683140fcef
thomasliu2019 <thomasyundaliu@gmail.com>
2026-01-30 11:03:08 -0500

    Add FeignConfig import to PostClient

--- src/main/java/com/example/socialProfile/client/PostClient.java
+++ src/main/java/com/example/socialProfile/client/PostClient.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.socialProfile.client.dto.PostResponse;
+import com.example.socialProfile.config.FeignConfig;
 
 @FeignClient(name = "post-service", url = "${services.post.url}", configuration = FeignConfig.class)
 public interface PostClient {
